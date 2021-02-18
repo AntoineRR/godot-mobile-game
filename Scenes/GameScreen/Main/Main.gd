46a34c2b3575fb1420f9b10e7c92474c03da8acc
antoine <antoine.romero-romero@student-cs.fr>
2021-02-18 16:29:28 +0100

    Add a pause menu

--- Scenes/GameScreen/Main/Main.gd
+++ Scenes/GameScreen/Main/Main.gd
@@ -2,3 +2,17 @@ extends Node
 
 func end_game():
 	GameManager.change_scene(GameManager.home_scene_path)
+
+func _on_Pause_pressed():
+	get_tree().paused = true
+	get_node("GUI/Popup").popup()
+
+
+func _on_Resume_pressed():
+	get_tree().paused = false
+	get_node("GUI/Popup").hide()
+
+
+func _on_Home_pressed():
+	get_tree().paused = false
+	GameManager.change_scene(GameManager.home_scene_path)
--- Scenes/GameScreen/Monster/Monster.gd
+++ Scenes/GameScreen/Monster/Monster.gd
@@ -1,8 +1,15 @@
 extends AnimatedSprite
 
+var current_height = 0
+var offset_step = 0
+
+func _ready():
+	current_height = frames.get_frame(animation, frame).get_size().y
+	offset_step = current_height / GameManager.max_player_health
+
 func _process(_delta):
 	var player = get_node("../Player")
-	var current_height = frames.get_frame(animation, frame).get_size().y
-	var offset = (GameManager.max_player_health - player.player_health) * 40
+	var offset = (GameManager.max_player_health - player.player_health) * offset_step
 	var y = player.position.y - get_viewport_rect().size.y/2 - current_height/2 + offset
-	position.y = y
+	if y > position.y:
+		position.y = y
