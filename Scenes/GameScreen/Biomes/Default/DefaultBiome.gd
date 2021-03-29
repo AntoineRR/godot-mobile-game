229d179033a6a261c9c6313b2b7426db43a88885
antoine <antoine.romero-romero@student-cs.fr>
2021-03-29 21:36:08 +0200

    Removes uneccessary code

--- Scenes/GameScreen/Biomes/Default/DefaultBiome.gd
+++ Scenes/GameScreen/Biomes/Default/DefaultBiome.gd
@@ -8,24 +8,6 @@ var tileset_path = "res://Scenes/GameScreen/Biomes/Default/Default.tres"
 ### Levels infos
 
 class Level1:
-	## Tiles infos
-	var walls = [
-		Biome.Tile.new(preload("res://Scenes/GameScreen/Biomes/Default/Tiles/Wall1.tscn"), 0.0),
-		Biome.Tile.new(preload("res://Scenes/GameScreen/Biomes/Default/Tiles/Wall2.tscn"), 1.0)
-	]
-	var obstacles = [
-		Biome.Tile.new(preload("res://Scenes/GameScreen/Biomes/Default/Tiles/Obstacle1.tscn"), 0.01),
-		Biome.Tile.new(preload("res://Scenes/GameScreen/Biomes/Default/Tiles/Obstacle2.tscn"), 0.02)
-	]
-	
-	## Entities infos
-	var enemies = [
-		Biome.Enemy.new(preload("res://Scenes/GameScreen/Enemies/Enemy1.tscn"), 0.01, Vector2(1,1))
-	]
-	var coins = [
-		Biome.Coin.new(preload("res://Scenes/GameScreen/Bonus/Money/Money.tscn"), 0.01)
-	]
-	
 	## Level data
 	var size = 20000
 	var monster_speed = 300
@@ -35,24 +17,6 @@ class Level1:
 	var player_max_speed = 700
 
 class Level2:
-	## Tiles infos
-	var walls = [
-		Biome.Tile.new(preload("res://Scenes/GameScreen/Biomes/Default/Tiles/Wall1.tscn"), 0.0),
-		Biome.Tile.new(preload("res://Scenes/GameScreen/Biomes/Default/Tiles/Wall2.tscn"), 1.0)
-	]
-	var obstacles = [
-		Biome.Tile.new(preload("res://Scenes/GameScreen/Biomes/Default/Tiles/Obstacle1.tscn"), 0.02),
-		Biome.Tile.new(preload("res://Scenes/GameScreen/Biomes/Default/Tiles/Obstacle2.tscn"), 0.04)
-	]
-	
-	## Entities infos
-	var enemies = [
-		Biome.Enemy.new(preload("res://Scenes/GameScreen/Enemies/Enemy1.tscn"), 0.02, Vector2(1,1))
-	]
-	var coins = [
-		Biome.Coin.new(preload("res://Scenes/GameScreen/Bonus/Money/Money.tscn"), 0.005)
-	]
-	
 	## Level data
 	var size = 30000
 	var monster_speed = 500
@@ -62,24 +26,6 @@ class Level2:
 	var player_max_speed = 850
 
 class Level3:
-	## Tiles infos
-	var walls = [
-		Biome.Tile.new(preload("res://Scenes/GameScreen/Biomes/Default/Tiles/Wall1.tscn"), 0.0),
-		Biome.Tile.new(preload("res://Scenes/GameScreen/Biomes/Default/Tiles/Wall2.tscn"), 1.0)
-	]
-	var obstacles = [
-		Biome.Tile.new(preload("res://Scenes/GameScreen/Biomes/Default/Tiles/Obstacle1.tscn"), 0.04),
-		Biome.Tile.new(preload("res://Scenes/GameScreen/Biomes/Default/Tiles/Obstacle2.tscn"), 0.08)
-	]
-	
-	## Entities infos
-	var enemies = [
-		Biome.Enemy.new(preload("res://Scenes/GameScreen/Enemies/Enemy1.tscn"), 0.04, Vector2(1,1))
-	]
-	var coins = [
-		Biome.Coin.new(preload("res://Scenes/GameScreen/Bonus/Money/Money.tscn"), 0.001)
-	]
-	
 	## Level data
 	var size = 50000
 	var monster_speed = 700
